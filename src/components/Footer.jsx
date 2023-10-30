13564a718a50905a675920a3c094b80cf0910214
Larocca <Larocca@RIVPRO1.net>
2023-10-30 11:48:20 -0400

    Added hover state color change and cleaned up code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,17 @@
 import React from "react";
 import { FaLinkedin, FaSquareXTwitter, FaGithub, FaCodepen, FaFreeCodeCamp } from "react-icons/fa6";
 import { SiStartrek } from "react-icons/si";
-import { IconContext } from "react-icons";
-
-
 
 function Footer() {
     return(
         <footer>
             <div className="social-icons">
-                <IconContext.Provider value={{ color: "white", className: "global-class-name" }}>
-                    <a href="https://www.linkedin.com/in/michaeljudelarocca" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
-                    <a href="https://www.twitter.com/mikejudelarocca" target="_blank" rel="noopener noreferrer"><FaSquareXTwitter /></a>
-                    <a href="https://github.com/MichaelLarocca" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
-                    <a href="https://codepen.io/Michael_Larocca" target="_blank" rel="noopener noreferrer"><FaCodepen /></a>
-                    <a href="https://www.freecodecamp.org/news/author/michael-larocca/" target="_blank" rel="noopener noreferrer"><FaFreeCodeCamp /></a>
-                    <a href="https://selftaughttxg.com/" target="_blank" rel="noopener noreferrer"><SiStartrek /></a>
-                </IconContext.Provider>
+                <a href="https://www.linkedin.com/in/michaeljudelarocca" target="_blank" rel="noopener noreferrer" className="icon"><FaLinkedin /></a>
+                <a href="https://www.twitter.com/mikejudelarocca" target="_blank" rel="noopener noreferrer"  className="icon"><FaSquareXTwitter /></a>
+                <a href="https://github.com/MichaelLarocca" target="_blank" rel="noopener noreferrer"  className="icon"><FaGithub /></a>
+                <a href="https://codepen.io/Michael_Larocca" target="_blank" rel="noopener noreferrer"  className="icon"><FaCodepen /></a>
+                <a href="https://www.freecodecamp.org/news/author/michael-larocca/" target="_blank" rel="noopener noreferrer"  className="icon"><FaFreeCodeCamp /></a>
+                <a href="https://selftaughttxg.com/" target="_blank" rel="noopener noreferrer"  className="icon"><SiStartrek /></a>
             </div>
         </footer>
     )
